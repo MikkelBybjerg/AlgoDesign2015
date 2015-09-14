b1da3a4fb874425792586a06ec6cd64915e4e738
MikkelBybjerg <mikkel.bybjerg@hotmail.com>
2015-09-14 18:25:51 +0200

    Add closest points framework

---
+++ ClosestPoints/ClosestPoints.java
@@ -0,0 +1,18 @@
+
+public class CP
+{
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	//  algorithm
+	
+	
+	//reads input as a list of 2d-points, i.e. [[1, 2],  [3, 4],  [5, 6]]
+	//returns a list with the indices of the two closest points in the input list, i.e. [2, 5]
+	public static int[] closestPoints(int[][] points)
+	{
+		return new int[2];
+	}
+}
